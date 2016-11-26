2834f6283de7e4bbf200e80f46ba291cb3f809e5
drif <drif@mail.ru>
2016-11-26 12:52:59 +0300

    initialized empty project

---
+++ StartSDK/StartSDK/StartSDK.h
@@ -0,0 +1,19 @@
+//
+//  StartSDK.h
+//  StartSDK
+//
+//  Created by drif on 11/26/16.
+//  Copyright © 2016 Payfort (http://payfort.com). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StartSDK.
+FOUNDATION_EXPORT double StartSDKVersionNumber;
+
+//! Project version string for StartSDK.
+FOUNDATION_EXPORT const unsigned char StartSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StartSDK/PublicHeader.h>
+
+
